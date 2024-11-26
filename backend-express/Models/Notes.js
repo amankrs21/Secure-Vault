de4d3b120149eba95984e641f21909b1cae1b52d
Aman Singh <amansingh844123@gmail.com>
2024-11-27 00:23:05 +0530

    feat: add Note model with schema for user notes

---
+++ backend-express/Models/Notes.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+module.exports = mongoose.model('UserNotes', NoteSchema);
