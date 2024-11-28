313b243b1a2498e7ddfc91ff51ee590bfbb06090
Aman Singh <amansingh844123@gmail.com>
2024-11-29 01:16:38 +0530

    SONARFIX: SH- add validation for required fields and improve note decryption checks in NotesController

--- backend-express/Controller/NotesController.js
+++ backend-express/Controller/NotesController.js
@@ -1,13 +1,44 @@
+const validator = require("validator");
+
 const UserNotes = require('../Models/Notes');
 const { currentUserID } = require("../Middleware/AuthUser.js");
 const { encrypt, decrypt } = require("./PasswordController.js");
+
+
+// Validate if required fields are provided
+const validateFields = (fields) => {
+    for (const [key, value] of Object.entries(fields)) {
+        if (!value) {
+            return { isValid: false, message: `${key} is required!` };
+        }
+    }
+    return { isValid: true };
+};
+
+
+// validate previous note with key
+const validatePreviousNote = async (userID, key) => {
+    const previousNote = await UserNotes.findOne({ createdBy: userID });
+    if (previousNote) {
+        try {
+            decrypt(previousNote.content, key);
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    }
+    return true;
+}
 
 
 // function to get all the notes of the user
 const getNotes = async (req, res) => {
     try {
         const { key } = req.body;
-        if (!key) { return res.status(400).json({ message: "Please provide the key!" }); }
+        const fieldValidation = validateFields({ key });
+        if (!fieldValidation.isValid) {
+            return res.status(400).json({ message: fieldValidation.message });
+        }
         const userID = await currentUserID(req, res);
         const notes = await UserNotes.find({ createdBy: userID });
         try {
@@ -28,18 +59,13 @@ const getNotes = async (req, res) => {
 const addNote = async (req, res) => {
     try {
         const { title, note, key } = req.body;
-        if (!title || !note || !key) {
-            return res.status(400).json({ message: "Please provide all required fields!" });
+        const fieldValidation = validateFields({ title, note, key });
+        if (!fieldValidation.isValid) {
+            return res.status(400).json({ message: fieldValidation.message });
         }
         const userID = await currentUserID(req, res);
-        const previousNote = await UserNotes.findOne({ createdBy: userID });
-        if (previousNote) {
-            try {
-                decrypt(previousNote.content, key);
-            } catch (error) {
-                console.error(error);
-                return res.status(400).json({ message: "Key is not able to decrypt the previous note!" });
-            }
+        if (!validatePreviousNote(userID, key)) {
+            return res.status(400).json({ message: "Key is not able to decrypt the previous note!" });
         }
         const encryptedNote = encrypt(note, key);
         const newNote = new UserNotes({
@@ -60,18 +86,13 @@ const addNote = async (req, res) => {
 const updateNote = async (req, res) => {
     try {
         const { id, title, note, key } = req.body;
-        if (!id || !title || !note || !key) {
-            return res.status(400).json({ message: "Please provide all required fields!" });
+        const fieldValidation = validateFields({ id, title, note, key });
+        if (!fieldValidation.isValid) {
+            return res.status(400).json({ message: fieldValidation.message });
         }
         const userID = await currentUserID(req, res);
-        const previousNote = await UserNotes.findOne({ createdBy: userID });
-        if (previousNote) {
-            try {
-                decrypt(previousNote.content, key);
-            } catch (error) {
-                console.error(error);
-                return res.status(400).json({ message: "Key is not able to decrypt the previous note!" });
-            }
+        if (!validatePreviousNote(userID, key)) {
+            return res.status(400).json({ message: "Key is not able to decrypt the previous note!" });
         }
         const encryptedNote = encrypt(note, key);
         const updatedNote = await UserNotes.findByIdAndUpdate(id, { title, content: encryptedNote }, { new: true });
@@ -87,15 +108,17 @@ const updateNote = async (req, res) => {
 const deleteNote = async (req, res) => {
     try {
         const { id, key } = req.body;
-        if (!id || !key) {
-            return res.status(400).json({ message: "Please provide all required fields!" });
+        const fieldValidation = validateFields({ id, key });
+        if (!fieldValidation.isValid) {
+            return res.status(400).json({ message: "Key is not able to decrypt the previous note!" });
         }
         const userID = await currentUserID(req, res);
-        const note = await UserNotes.findOne({ _id: id, createdBy: userID });
+        const sanitizedId = validator.escape(id);
+        const note = await UserNotes.findOne({ _id: sanitizedId, createdBy: userID });
         if (!note) {
             return res.status(404).json({ message: "Note not found!" });
         }
-        await UserNotes.findByIdAndDelete(id);
+        await UserNotes.findByIdAndDelete(sanitizedId);
         return res.status(200).json({ message: "Note Deleted Successfully!" });
     } catch (error) {
         console.error(error);
