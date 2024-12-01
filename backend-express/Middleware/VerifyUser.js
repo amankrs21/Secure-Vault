381c8f526f325459dff52eea636f538e1aa0fd88
Aman Singh <amansingh844123@gmail.com>
2024-12-02 01:48:42 +0530

    refactor: remove AuthUser middleware and replace with separate VerifyUser, VerifyAdmin, and CurrentUser middleware using UserDB

---
+++ backend-express/Middleware/VerifyUser.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+
+const UserDB = require("../Models/UserDB.js");
+
+const SecretKey = process.env.SECRET_KEY;
+
+
+const VerifyUser = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.slice(7);
+        if (!token) {
+            return res.status(401).json({ message: "Token is not provided or invalid" });
+        }
+        try {
+            const decoded = jwt.verify(token, SecretKey);
+            const user = await UserDB.findById(decoded?.id);
+            if (!user || ![0, 1].includes(user.role)) {
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+            next();
+        } catch (error) {
+            if (error.name === 'TokenExpiredError') {
+                return res.status(401).json({ message: "Token has expired" });
+            } else if (error.name === 'JsonWebTokenError') {
+                return res.status(401).json({ message: "Invalid token" });
+            }
+            throw error;
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+module.exports = VerifyUser;
