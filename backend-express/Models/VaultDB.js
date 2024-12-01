ad14c0ff075b85d189d6d0bebb36cd25c084917c
Aman Singh <amansingh844123@gmail.com>
2024-12-02 01:35:55 +0530

    feat: add VaultDB model for secure storage of user credentials

---
+++ backend-express/Models/VaultDB.js
@@ -0,0 +1,28 @@
+const mongo = require('mongoose');
+
+const VaultDB = new mongo.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserDB',
+        required: true
+    }
+});
+
+module.exports = mongo.model('VaultDB', VaultDB);
