1d34697b2d565693a09909c170b30a806a101379
Aman Singh <amansingh844123@gmail.com>
2024-12-29 23:31:15 +0530

    feat: add AccountPass and AccountProfile components for password and profile management

---
+++ client/src/pages/account/AccountPass.jsx
@@ -0,0 +1,114 @@
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import { TextField, Button, InputAdornment, IconButton, Typography } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+
+export default function AccountPass() {
+
+    const [btnDisabled, setBtnDisabled] = useState(true);
+
+    const [passwords, setPasswords] = useState({
+        currentPassword: '',
+        newPassword: '',
+        confirmPassword: '',
+    });
+
+    const [showPassword, setShowPassword] = useState({
+        current: false,
+        new: false,
+        confirm: false,
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setPasswords((prev) => ({ ...prev, [name]: value }));
+        btnDisabled && setBtnDisabled(false);
+    };
+
+    const handleTogglePassword = (field) => {
+        setShowPassword((prev) => ({ ...prev, [field]: !prev[field] }));
+    };
+
+    const handleSubmit = () => {
+        console.log('Password Change Request:', passwords);
+    };
+
+    return (
+        <div>
+            <Typography variant="h5" color='primary' fontWeight={700} marginY={2}>
+                Change Password
+            </Typography>
+            <Typography variant="caption" color="error">
+                *You can update your password here.
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit}>
+                <TextField
+                    required
+                    fullWidth
+                    name="currentPassword"
+                    label="Current Password"
+                    type={showPassword.current ? 'text' : 'password'}
+                    value={passwords.currentPassword}
+                    onChange={handleChange}
+                    sx={{ marginY: 3 }}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={() => handleTogglePassword('current')}>
+                                    {showPassword.current ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <TextField
+                    required
+                    fullWidth
+                    name="newPassword"
+                    label="New Password"
+                    type={showPassword.new ? 'text' : 'password'}
+                    value={passwords.newPassword}
+                    onChange={handleChange}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={() => handleTogglePassword('new')}>
+                                    {showPassword.new ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <TextField
+                    required
+                    fullWidth
+                    name="confirmPassword"
+                    label="Confirm New Password"
+                    type={showPassword.confirm ? 'text' : 'password'}
+                    value={passwords.confirmPassword}
+                    onChange={handleChange}
+                    sx={{ marginY: 3 }}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={() => handleTogglePassword('confirm')}>
+                                    {showPassword.confirm ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <Button
+                    type='submit'
+                    color="secondary"
+                    variant="contained"
+                    disabled={btnDisabled}
+                    sx={{ marginTop: 1, float: 'right' }}
+                >
+                    Update Password
+                </Button>
+            </Box>
+
+        </div>
+    );
+}
---
+++ client/src/pages/account/AccountProfile.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid2';
+import { TextField, Typography, Button } from '@mui/material';
+import { Save } from '@mui/icons-material';
+
+export default function AccountProfile({ userData }) {
+
+    const [btnDisabled, setBtnDisabled] = useState(true);
+
+    const [formValues, setFormValues] = useState({
+        id: userData?._id || '',
+        name: userData?.name || '',
+        dateOfBirth: userData?.dateOfBirth || '',
+        secretAnswer: atob(userData?.secretAnswer) || '',
+    });
+
+    const handleChange = (e) => {
+        btnDisabled && setBtnDisabled(false);
+        const { name, value } = e.target;
+        setFormValues((prevValues) => ({
+            ...prevValues,
+            [name]: value,
+        }));
+    };
+
+    const handleSave = () => {
+        formValues.secretAnswer = btoa(formValues.secretAnswer);
+        console.log("Saving new data:", formValues);
+    };
+
+    return (
+        <Grid container>
+            <Grid size={{ xs: 12, md: 4 }} className="account-profile-intro">
+                <div className='account-profile-img'>
+                    <img src={`https://robohash.org/${userData.name}`} alt={userData.name} />
+                </div>
+                <Typography gutterBottom variant="h5" fontWeight={700}>
+                    Hi {userData.name ? (userData?.name.split(' ')[0]) : "User"} 👋
+                </Typography>
+                <Typography variant="substitle1" marginTop={3}>
+                    created on:&nbsp;
+                    <b style={{ color: '#1976d2' }}>{new Date(userData.createdAt).toDateString()}</b>
+                </Typography>
+            </Grid>
+            <Grid size={{ xs: 12, md: 8 }}>
+                <Typography variant="h5" color='primary' fontWeight={700} marginY={2}>
+                    Update Profile
+                </Typography>
+                <Typography variant="caption" color="error">
+                    *You can only update your name, secret answer, and date of birth here.
+                </Typography>
+                <br />
+                <TextField
+                    disabled
+                    fullWidth
+                    name="email"
+                    label="Email"
+                    value={userData.email}
+                    sx={{ marginY: 3 }}
+                />
+                <TextField
+                    fullWidth
+                    required
+                    name="name"
+                    label="Full Name"
+                    variant="outlined"
+                    value={formValues.name}
+                    onChange={handleChange}
+                />
+                <TextField
+                    fullWidth
+                    name="secretAnswer"
+                    label="Favorite Place"
+                    value={formValues.secretAnswer}
+                    onChange={handleChange}
+                    sx={{ marginY: 3 }}
+                />
+                <TextField
+                    fullWidth
+                    name="dateOfBirth"
+                    label="Date of Birth"
+                    type="date"
+                    value={formValues.dateOfBirth}
+                    onChange={handleChange}
+                />
+
+                <Button
+                    disabled={btnDisabled}
+                    color="primary"
+                    variant="contained"
+                    onClick={handleSave}
+                    sx={{ marginTop: 4, float: 'right' }}
+                    startIcon={<Save />}
+                >
+                    Update Changes
+                </Button>
+            </Grid>
+        </Grid>
+    );
+}
+
+AccountProfile.propTypes = {
+    userData: PropTypes.object
+};
