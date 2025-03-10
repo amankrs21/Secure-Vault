55e3f6a9c5adabc64ed453dafca31824624adf52
Aman Singh <amankrs212@gmail.com>
2025-03-11 02:38:04 +0530

    feat: implement new routing structure with ErrorBoundary and Loading context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,28 @@
-import {
-  BrowserRouter, Routes, Route, Navigate
-} from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, Slide } from 'react-toastify';
 
-import Home from "./pages/home/Home";
-import Vault from "./pages/vault/Vault";
-import Login from "./pages/login/Login";
-import Account from "./pages/account/Account";
-import Journal from "./pages/journal/Journal";
-import Register from "./pages/register/Register";
-import ServerUnavl from "./pages/error/ServerUnavl";
-import PageNotFound from "./pages/error/PageNotFound";
-import PrivateRoutes from "./middleware/PrivateRoutes";
-import Collaborate from "./pages/collaborate/Collaborate";
+import Router from "./Router";
+import Loading from './components/Loading';
+import ErrorPage from './components/ErrorPage';
+import ErrorBoundary from './middleware/ErrorBoundary';
+import { LoadingProvider } from './contexts/LoadingContext';
 
 
 export default function App() {
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Navigate to='/login' />} />
-        <Route path='*' element={<Navigate to='/404' />} />
-        <Route path='/login' element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/404" element={<PageNotFound />} />
-        <Route path="/503" element={<ServerUnavl />} />
-        <Route path="/" element={<PrivateRoutes />}>
-          <Route path='/home' element={<Home />} />
-          <Route path='/vault' element={<Vault />} />
-          <Route path='/journal' element={<Journal />} />
-          <Route path='/account' element={<Account />} />
-          <Route path='/collaborate' element={<Collaborate />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary fallback={<ErrorPage />}>
+      <LoadingProvider>
+        <Loading />
+        <Router />
+      </LoadingProvider>
+      <ToastContainer
+        theme="colored"
+        draggable={false}
+        transition={Slide}
+        hideProgressBar={true}
+        position="bottom-right"
+      />
+    </ErrorBoundary>
   );
 };
--- client/src/middleware/PrivateRoutes.jsx
+++ client/src/middleware/PrivateRoutes.jsx
@@ -1,42 +1,37 @@
+import { Outlet } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
 
-import AuthUser from './AuthProvider';
-import Header from '../pages/header/Header';
+import { useAuth } from '../hooks/useAuth';
+
+import Header from '../layout/Header';
 import KeySetupModal from '../components/key/KeySetupModal';
 import KeyAccessModal from '../components/key/KeyAccessModal';
-import { useLoading } from '../components/loading/useLoading';
 
 
+// PrivateRoutes component to protect routes
 export default function PrivateRoutes() {
-    const navigate = useNavigate();
-    const { isValidToken } = AuthUser();
-    const { setLoading } = useLoading();
+
+    const { isAuthLoading, isAuthenticated, http, logout } = useAuth();
+
     const [openSetup, setOpenSetup] = useState(false);
     const [openAccess, setOpenAccess] = useState(false);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        if (!isAuthenticated) logout();
+        if (isAuthLoading || !http.defaults.headers.common.Authorization) return;
+    }, [isAuthLoading, isAuthenticated, http, logout]);
 
     useEffect(() => {
         const checkAuth = async () => {
-            setLoading(true);
             const eKey = localStorage.getItem("eKey");
-            const token = localStorage.getItem("token");
             const isKeySet = localStorage.getItem("isKeySet");
-            if (!token || !(await isValidToken(token))) {
-                localStorage.clear();
-                navigate('/login');
-            } else {
-                setIsAuthenticated(true);
-                if (!eKey) {
-                    if (isKeySet === 'false') setOpenSetup(true);
-                    else setOpenAccess(true);
-                }
+            if (!eKey) {
+                if (isKeySet === 'false') setOpenSetup(true);
+                else setOpenAccess(true);
             }
-            setLoading(false);
         };
-
         checkAuth();
-    }, [navigate, setLoading]);
+    }, []);
 
     if (!isAuthenticated) {
         return null;
@@ -44,12 +39,18 @@ export default function PrivateRoutes() {
 
     return (
         <>
-            <Header />
-            <div style={{ marginTop: '9vh', height: '91vh', overflowY: 'auto' }}>
-                {openSetup && <KeySetupModal openSetup={openSetup} setOpenSetup={setOpenSetup} />}
-                {openAccess && <KeyAccessModal openAccess={openAccess} setOpenAccess={setOpenAccess} />}
-                <Outlet />
-            </div>
+            {!isAuthLoading && isAuthenticated && http.defaults.headers.common.Authorization ? (
+                <>
+                    <Header />
+                    <div style={{ marginTop: '8vh', height: '92vh', overflowY: 'auto', paddingTop: '5px' }}>
+                        {openSetup && <KeySetupModal openSetup={openSetup} setOpenSetup={setOpenSetup} />}
+                        {openAccess && <KeyAccessModal openAccess={openAccess} setOpenAccess={setOpenAccess} />}
+                        <Outlet />
+                    </div>
+                </>
+            ) :
+                null
+            }
         </>
     );
 };
