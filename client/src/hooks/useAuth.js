e6a597ce039a1d3a5071af17c19463def54b4021
Aman Singh <amankrs212@gmail.com>
2025-03-12 03:06:51 +0530

    feat: add ErrorPage component and implement AuthContext for authentication management

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+
+import { AuthContext } from '../contexts/AuthContext';
+
+
+// Custom hook to use the AuthContext
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
