ab08de7462437a7a68c734bdcf7066845bd146dc
Aman Singh <amankrs212@gmail.com>
2025-03-12 03:04:14 +0530

    feat: implement loading context and refactor loading component

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,32 @@
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import { useLoading } from '../hooks/useLoading';
+
+
+// Loading spinner
+export default function Loading() {
+    const { loading } = useLoading();
+
+    if (!loading) {
+        return null;
+    }
+
+    return (
+        <Box sx={{
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 9999,
+            position: 'absolute',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backdropFilter: 'blur(1px)',
+            backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        }}>
+            <CircularProgress color="secondary" size="3rem" />
+        </Box>
+    );
+}
