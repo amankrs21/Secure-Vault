6a3526bb0488a3b5121b61ed0c18fb1ddcf0bc5a
Aman Singh <amankrs212@gmail.com>
2025-03-14 00:40:58 +0530

    feat: update error messages for form validation; refactor app and index files for improved structure and CORS configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,18 @@
 const http = require("http");
 
 const app = require('./app');
+const mongoConnect = require("./config/mongo.config.js");
+
 
 const port = process.env.PORT ?? 3000;
+
+
+// Connect to the database
+mongoConnect();
+
 
 // Create server with Express app and HTTP
 const server = http.createServer(app);
 server.listen(port, "0.0.0.0", () => {
-    console.info(`Express Server started on PORT: ${port}`);
+    console.info(`\x1b[36m☠️  SERVER STARTED AT PORT: ${port}\x1b[0m`);
 });
